78c47210d6b09ffa6aad0ba848228687dd7d7a13
huda_aeda <44691789+hudaaeda10@users.noreply.github.com>
2021-04-17 13:19:40 +0700

    pertemuan 3

---
+++ src/components/HelloWorldComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HelloWorldComponent.css";
+
+const HelloWorldComponent = () => {
+  return (
+    <div>
+      <p className="text-p">Stateless Component Function</p>
+    </div>
+  );
+};
+
+export default HelloWorldComponent;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import HelloWorldComponent from "./components/HelloWorldComponent";
+import StateFullComponent from "./container/StateFullComponent";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HelloWorldComponent />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
